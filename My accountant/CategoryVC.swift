8efb8b4ef6733d8ddbacac2c7be71bac662f18e5
Yulia <yuliyalapenak@gmail.com>
2022-07-24 16:40:37 +0300

    ExpenseTransactionScreen add Alert add doneButton

--- My accountant/CategoryVC.swift
+++ My accountant/CategoryVC.swift
@@ -31,6 +31,7 @@ class CategoryVC : UIViewController {
 
 
 }
+
     extension CategoryVC: UITableViewDelegate, UITableViewDataSource {
     
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- My accountant/ExpenseTransactionScreen.swift
+++ My accountant/ExpenseTransactionScreen.swift
@@ -7,78 +7,159 @@
 
 import UIKit
 
-class ExpenseTransactionScreen: UIViewController {
-    
-   
-    @IBOutlet weak var displayLabel: UILabel!
-    var stillTyping = false
+protocol TransactionHandler {
+    func newTransaction(amount: Float, category: String, note: String, date: Date, newTransaction: Bool)
+}
 
+
+class ExpenseTransactionScreen: UIViewController, Category, UITextFieldDelegate  {
+
+    @IBOutlet var amountTextField: UITextField!
     @IBOutlet var categoryTextField: UITextField!
     @IBOutlet var descriptionTextField: UITextField!
     @IBOutlet var dateTextField: UITextField!
     @IBOutlet var datePicker: UIDatePicker!
 
-//
-//    @IBOutlet var numberFromKeyboard: [UIButton]! {
-//        //чтоб задать свойства кнопке, вызываем наблюдателя свойств didSet и пишем условие, что для кнопки указываем
-//        didSet {
-//            for button in numberFromKeyboard {
-//                button.layer.cornerRadius = 11
+    var newTransaction: Bool = true
+    var delegate: TransactionHandler?
+    
+    var amount = Float()
+    var category = String()
+    var note = String()
+    var date = Date()
+    
+    
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            if newTransaction == false {
+                datePicker.date = date
+                updateDate()
+             
+                
+                DispatchQueue.main.async {
+                    self.amountTextField.text = "\(self.amount)"
+                    self.categoryTextField.text = self.category
+                    self.descriptionTextField.text = self.note
+                }
+            }
+            
+            datePicker.isHidden = true
+            updateDate()
+            
+            datePicker.addTarget(self, action: #selector(updateDate), for: .valueChanged)
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showDatePicker))
+            dateTextField.addGestureRecognizer(tapGesture)
+            
+            amountTextField.delegate = self
+            categoryTextField.delegate = self
+            descriptionTextField.delegate = self
+            
+            amountTextField.addDoneButtonToKeyboard(myAction:  #selector(self.amountTextField.resignFirstResponder))
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool
+        {
+            amountTextField.resignFirstResponder()
+            categoryTextField.resignFirstResponder()
+            descriptionTextField.resignFirstResponder()
+            return true
+        }
+   
+        
+        @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+            let floatNumber = (amountTextField.text! as NSString).floatValue
+            if floatNumber == 0 {
+                let alert = UIAlertController(title: "Error", message: "Enter amount", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default)
+                
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
+            } else {
+                delegate?.newTransaction(amount: floatNumber, category: categoryTextField.text!, note: descriptionTextField.text!, date: datePicker.date, newTransaction: newTransaction)
+                navigationController?.popToRootViewController(animated: true)
+            }
+        }
+        
+        @objc func updateDate() {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd MMM YYYY"
+            let selectedDate = dateFormatter.string(from: datePicker.date)
+            dateTextField.text = selectedDate
+        }
+        
+        @objc func showDatePicker() {
+            datePicker.isHidden = false
+            amountTextField.endEditing(true)
+            categoryTextField.endEditing(true)
+            descriptionTextField.endEditing(true)
+            dateTextField.endEditing(true)
+        }
+        
+        func selectedCategory(category: String) {
+            categoryTextField.text = category
+        }
+        
+//        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            if segue.identifier == "showCategories" {
+//                let destinationVC = segue.destination as! CategoryVC
+//                destinationVC.delegate = self
 //            }
 //        }
-//    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
-    
-    
-    private func loadData(){
-        let request = Transaction.fetchRequest()
-        request.sortDescriptors = [NSSortDescriptor(key: #keyPath(Transaction.amount
-                                                                 ), ascending: true )]
-        let transactions = try? CoreDataService.mainContext.fetch(request)
-    }
-    
-    
-    @IBAction func saveDidTap(){
-        let amount = displayLabel.text
-        let note = descriptionTextField.text
-        let category = categoryTextField.text
-        let date = datePicker.date
-        
-        let transactions = Transaction(context: CoreDataService.mainContext)
-        
-//
-        
-        transactions.note = note
-        transactions.category = category
-        transactions.date = date
-        
-        CoreDataService.saveContext()
-    }
-    
-    @IBAction func numberPressed(_ sender: UIButton) {
-        let number = sender.currentTitle!
-        
-        if stillTyping {
-            if displayLabel.text!.count < 10  {
-                displayLabel.text = displayLabel.text! + number
-            }
-        } else {
-            displayLabel.text = number
-           stillTyping = true
+
+    extension UITextField{
+        func addDoneButtonToKeyboard(myAction:Selector?){
+            let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 300, height: 40))
+            doneToolbar.barStyle = UIBarStyle.default
+            
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+            
+            let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: myAction)
+            
+            var items = [UIBarButtonItem]()
+            items.append(flexSpace)
+            items.append(done)
+            
+            doneToolbar.items = items
+            doneToolbar.sizeToFit()
+            
+            self.inputAccessoryView = doneToolbar
         }
-    
-    //сделаем так, чтобы количество символов было ограниченно
-    // убираем ноль при нажатии на любую клавишу для этого создаем переменную с булевым значением stillTyping
-}
-    
-    @IBAction func resetButton(_ sender: UIButton) {
-        displayLabel.text = "0"
-        stillTyping = false
     }
+
     
- 
-}
+//    private func loadData(){
+//        let request = Transaction.fetchRequest()
+//        request.sortDescriptors = [NSSortDescriptor(key: #keyPath(Transaction.amount
+//                                                                 ), ascending: true )]
+//        let transactions = try? CoreDataService.mainContext.fetch(request)
+//    }
+//    
+//    
+//    @IBAction func saveDidTap(){
+//        let amount = displayLabel.text
+//        let note = descriptionTextField.text
+//        let category = categoryTextField.text
+//        let date = datePicker.date
+//        
+//        let transactions = Transaction(context: CoreDataService.mainContext)
+//        
+////
+//        
+//        transactions.note = note
+//        transactions.category = category
+//        transactions.date = date
+//        
+//        CoreDataService.saveContext()
+//    }
+// 
+    
+
+    
+  
+   
+
+
+    
